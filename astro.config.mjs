f7a14c0bd06ce7c59ee88969a2a5f74586552a80
madPO <madpo.s.a@gmail.com>
2025-11-21 23:12:00 +0300

    feat: added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://madpo.github.io",
+
   vite: {
     plugins: [],
   },
+
   markdown: {
     shikiConfig: {
       theme: "github-dark-default",
     },
   },
+
+  integrations: [sitemap()],
 });
