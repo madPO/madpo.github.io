c98bdf753e2d9dd10dd9ad01b3813c3afc3e0127
madPO <madpo.s.a@gmail.com>
2026-02-01 17:56:08 +0300

    small fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://madpo.github.io/",
+  site: "https://madpo.me/",
 
   vite: {
     plugins: [],
